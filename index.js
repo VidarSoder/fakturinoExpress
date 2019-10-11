439321e706b185f71c43d2541ce71d661eb8f15e
VidarSoder <vidar.soder@gmail.com>
2019-10-11 09:55:56 +0200

    version 2

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
 var cors = require('cors')
-const port = 5000
+const path = require('path');
+const http = require('http');
 
+const port = 5000
+const server = http.Server(app);
 
 app.use(cors())
+
+app.use('/', express.static(path.join(__dirname, 'expressFakturino')));
 
 app.get('/welcome', (req, res) => {
     
